16e47e36a8a98a86cf31c9fe62fda54799622023
Chicj <chic.jordan@yahoo.com>
2017-10-26 13:52:55 -0800

    Added IncrementVcore to control freq

--- peripheral.c
+++ peripheral.c
@@ -28,9 +28,47 @@ void Radio_SPI_setup(void){
   radio_SPI_desel();                 // Ensure CS for CC1101 is disabled
 
   //Set pins for SPI usage
-  //P4SEL0 |= RADIO_PINS_SPI;
+   P4SEL0 |= RADIO_PINS_SPI;
 }
+//******************************************************** Clock **************************************
+ //TODO test this
+ void ClockSetUp(void){ // will set MCLK to 20 MHz
+  unsigned short   old_level; 
 
+  // read the previous Vcore level 
+  old_level = PMMCTL0 & (PMMCOREV_3); 
+  //increment Voltage in the core until we get to level 3
+  while (old_level < PMMCOREV_3) {
+      IncrementVcore();
+  }
+  // set MCLK to 25 MHz (default SELREF for FLLREFCLK is XT1CLK = 32*1024 = 32768 Hz = 32.768 KHz)
+  UCSCTL2 = 762; // Setting the freq multiplication factor * 1024
+  UCSCTL1 |= DCORSEL_6;     // This sets the DCO frequency pg26 data sheet
+  UCSCTL4 |= SELM__DCOCLKDIV + SELM__DCOCLKDIV;  // set output of FLLREFCLK --> DCOCLKDIV to input of MCLK and SMCLK  <-- This is default
+ 
+  /*// setup MSP430f5xxx breakout board MCLK and SMCLK test pins for scope testing
+  P11DIR  |= BIT1;  // Set Port 11 pin 1 direction as an output
+  P11SEL  |= BIT1; // Set port 11 pin 1 as MCLK signal 
+  
+  P11DIR  |= BIT2;  // Set Port 11 pin 2 direction as an output
+  P11SEL  |= BIT2; // Set port 11 pin 2 as SMCLK signal 
+  */
+
+  // board PWM pin setups
+  P8DIR |= BIT6;  // set up P8.6 as an OUT1 in TimerA1  25%
+  P7DIR |= BIT3;  // set up P7.3 as an OUT2 TimerA1 75%
+
+  P8SEL |= BIT6;  // set up P8.6 as an Output mode 
+  P7SEL |= BIT3;  // set up P7.3 as an Output mode 
+
+  // disable SMCLK requests
+  UCSCTL8 |= ~SMCLKREQEN;
+
+  // TAxCCRn = Content of capture/compare register n will set the count up to val
+  TA1CCR0 = TOTALCOUNT; // set count up val 
+  TA1CCR1 = 94;   //P8.6  75%
+  TA1CCR2 = 32;   //P7.3  25%
+ }
 
 //******************************************************** UART **************************************
 // UART is connected UART_Tx P3.5 UCA1, UART_Rx P3.4 
